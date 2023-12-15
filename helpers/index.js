2ddd61c76105ca48db86fffa0202a422037298d1
Sahil-Mund <Sahil.Mund@tryantler.com>
2023-12-15 13:23:46 +0530

    fixed idx column

--- helpers/index.js
+++ helpers/index.js
@@ -1,16 +1,18 @@
+var categoryIdx = 1;
 
 module.exports.applyAggregation = (data) => {
-  const result = data.reduce((acc, current,index) => {
-    const key = `${current.Category}-${index+1}`;
+  const result = data.reduce((acc, current) => {
+    const key = `${current.Category}`;
 
     // group by category & idx and push all the items with same categories to its items array
     if (!acc[key]) {
       acc[key] = {
         Category: current.Category,
-        idx: index+1,
-        side: index%2 == 0 ? 'left' : 'right',
+        idx: categoryIdx,
+        side: (categoryIdx) % 2 !== 0 ? 'left' : 'right',
         items: [],
       };
+      categoryIdx++;
     }
   
 
